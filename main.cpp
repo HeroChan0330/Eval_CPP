646c515475a7947c3d92cc22f6f5a850c6e4a6fb
HeroChan <592061719@qq.com>
2018-04-20 13:20:19 +0800

    resistance

--- main.cpp
+++ main.cpp
@@ -5,88 +5,32 @@
 
 using namespace std;
 
-
-//Dijkstra双栈计算器
-double Compute(char*str){//计算的表达式需要用左右括号包围 且每2个操作数要用括号包围
-    Stack<char> opStack;
-    Stack<double> numStack;
-    while(*str){
-        if((*str)=='(');//左括号不处理
-        else if((*str)=='+'||(*str)=='-'||(*str)=='*'||(*str)=='/'){//四则运算符号入栈
-            opStack.Push(*str);
-        }
-        else if((*str)==')'){//右括号出栈
-            double v=numStack.Pop();
-            char op=opStack.Pop();
-            switch(op){
-                case '+':
-                    v=numStack.Pop()+v;
-                    break;
-                case '-':
-                    v=numStack.Pop()-v;
-                    break;
-                case '*':
-                    v=numStack.Pop()*v;
-                    break;
-                case '/':
-                    v=numStack.Pop()/v;
-                    break;
-            }
-            numStack.Push(v);
-        }
-        else{//获取double
-            int i=0;
-            while((str[i]>=48&&str[i]<=57)||str[i]=='.'){
-                i++;
-            }
-            double num;
-            sscanf(str,"%lf",&num);
-            numStack.Push(num);
-            printf("%lf\n",num);
-            str+=i-1;//将指针移到数字末尾
-        }
-        str++;
-    }
-    return numStack.Pop();
-}
-
-
-
 //获取符号的优先级
 int GetPriority(char op){
     switch(op){
     case '(':
         return 0;
         break;
-    case '+':case '-':
+    case '+':
         return 1;
         break;
-    case '*':case '/':
+    case '/':
         return 2;
         break;
     }
     return 0;
 }
-//对2个操作数进行四则运算
 double SingleCompute(double a,double b,char op){
-    switch(op){
-    case '+':
+    if(op=='+'){
         return a+b;
-        break;
-    case '-':
-        return a-b;
-        break;
-    case '*':
-        return a*b;
-        break;
-    case '/':
-        return a/b;
-        break;
+    }
+    else if(op=='/'){
+        return 1/(1/a+1/b);
     }
 }
-//优化的Dijkstra双栈计算器 http://blog.csdn.net/xieyihua1994/article/details/51644720
 
-double Compute2(char*str){//计算的表达式需要用左右括号包围 且每2个操作数要用括号包围
+
+double ResistanceCompute(char*str){//电阻计算器
     Stack<char> opStack;
     Stack<double> numStack;
     while(*str){
@@ -94,7 +38,7 @@ double Compute2(char*str){//计算的表达式需要用左右括号包围 且每2个操作数要用括号
             opStack.Push(*str);
             str++;
         }
-        else if((*str)=='+'||(*str)=='-'||(*str)=='*'||(*str)=='/'){//四则运算符号入栈
+        else if((*str)=='+'||(*str)=='/'){//运算符号入栈
             if(GetPriority(*str)>GetPriority(opStack.Peek()))
                 opStack.Push(*str);//直接入栈
             else{//把前面高优先级的式子算完再入栈
@@ -137,7 +81,7 @@ int main(int args,char*argv[])
         char expression[50]={0};
         expression[0]='(';
         snprintf(expression+1,48,"%s)",argv[1]);
-        double result=Compute2(expression);
+        double result=ResistanceCompute(expression);
         printf("%s=%lf\n\n",expression,result);
     }
     else{
@@ -147,15 +91,9 @@ int main(int args,char*argv[])
             scanf("%s",expression+1);
             expression[strlen(expression)]=')';
             //对表达式进行格式化
-            double result=Compute2(expression);
+            double result=ResistanceCompute(expression);
             printf("%s=%lf\n\n",expression,result);
         }
     }
-    /*
-    char*str="(1.76+98.25)";
-    printf("result:%lf\n",Compute(str));
-    */
-
-
     return 0;
 }
